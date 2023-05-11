7020ad28c7d88406885333fb65c70b9d77ef6039
Andrew Steshenko <steshmen@gmail.com>
2023-05-11 13:59:45 +0300

    chnage function

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 console.log('hello github');
 
-const main = (value) => {
-  return value;
+const main = (name, value) => {
+
+  if(name === 'main') {
+    return value;
+  }
+  return 'name is not found';
 }
